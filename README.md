abaca8f04634ac000ab9b74827fe3c4a92877e88
Stephen-Treasure <stephentreasure8@gmail.com>
2022-08-02 00:11:38 +1000

    only retrives 10 books issue

---
+++ README.md
@@ -0,0 +1,6 @@
+# Google Books UI
+
+Google book search API UI
+
+Didn't realise until late that it was only retreiving 10 books at a time as I had max results set to 5 while I got it to work and how I wanted it to look and can't seem to figure out what the issue is.
+Cause of this I ran out of time to seperate files into different folders.
--- index.js
+++ index.js
@@ -1,69 +1,22 @@
-const getData = async (searchValue) => {
-    const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchValue}&maxResults=5`,
-    );
-    const data = await response.json();
-    // console.log(data.items);
-
-    // const { volumeInfo } = data.items;
-    // console.log(volumeInfo.title);
-
-    // data.items.map((e) => {
-    //     return {
-    //         title: e.volumeInfo.title,
-    //     };
-    // });
-
-    return data.items.map((e) => {
-        return {
-            image: e.volumeInfo.imageLinks.thumbnail || "image unavaiable",
-            title: e.volumeInfo.title,
-            authors: e.volumeInfo.authors.join(",") || "authors unavaiable",
-            description: e.volumeInfo.description,
-        };
-    });
-    // return data.items.forEach((key) => {
-    //     key.volumeInfo.title;
-    // });
-};
-
 document.getElementById("btn").addEventListener("click", async () => {
-    const inf = await getData2(search.value);
-    // console.log(inf);
+    const inf = await getData(search.value);
+    console.log(inf);
     inf.forEach((book) => createCard(book));
-
-    // const title = document.createElement("h2");
-    // title.classList.add("title");
-    // title.innerText = getData().title;
-
-    // document.getElementById("title").value = getData(search.value).title;
 });
 
-// const card = document.createElement('div').classList('card');
-// card.appendChild(document.createElement('h2').add('title'));
-
-// const title = document.createElement("h3");
-// title.classList.add("title");
-// title.innerHTML = getData().title;
-
-// const authors = document.createElement("p");
-// authors.classList.add("authors");
-
-// const description = document.createElement("p");
-// description.classList.add("description");
-
-const getData2 = async (searchValue) => {
+const getData = async (searchValue) => {
     const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchValue}&maxResults=20`,
+        `https://www.googleapis.com/books/v1/volumes?q=${searchValue}`,
     );
     const data = await response.json();
 
     let books = data.items.map((book) => {
         return {
-            image: book.volumeInfo.imageLinks.thumbnail || "image unavaiable",
             title: book.volumeInfo.title,
             authors: book.volumeInfo.authors.join(",") || "authors unavaiable",
-            description: book.volumeInfo.description,
+            description:
+                book.volumeInfo.description || "description unavaiable",
+            image: book.volumeInfo.imageLinks,
         };
     });
 
@@ -73,10 +26,6 @@ const getData2 = async (searchValue) => {
 const createCard = (book) => {
     const card = document.createElement("div");
     card.classList.add("card");
-
-    const image = document.createElement("img");
-    image.classList.add("image");
-    image.src = book.image || "src / img / Image - Unavailable.jpg";
 
     const title = document.createElement("h3");
     title.classList.add("title");
@@ -90,10 +39,14 @@ const createCard = (book) => {
     description.classList.add("description");
     description.innerText = book.description;
 
-    card.appendChild(image);
+    const image = document.createElement("img");
+    image.classList.add("image");
+    image.src = book.image.thumbnail;
+
     card.appendChild(title);
+    card.appendChild(image);
     card.appendChild(authors);
     card.appendChild(description);
 
-    document.getElementById("card-container").appendChild(card);
+    document.getElementById("cardContainer").appendChild(card);
 };
