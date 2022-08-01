58e781ce84469b5866d30f967e44e7ed004e554c
Stephen-Treasure <stephentreasure8@gmail.com>
2022-08-01 11:13:44 +1000

    commit

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-// const Load = async (url) => {
-//     const response = await fetch(url);
-//     console.log("url");
-
-//     return response.json();
-// };
-
-const url_api =
-    "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyA8bWDWKfJ5UztmwkbAB4XiT8Md30xjV50";
-
-const apiKey = "AIzaSyA8bWDWKfJ5UztmwkbAB4XiT8Md30xjV50";
-
-// let search = document.getElementById("search").value;
-
-const url_api2 =
-    "https://www.googleapis.com/books/v1/volumes?q=&key=AIzaSyA8bWDWKfJ5UztmwkbAB4XiT8Md30xjV50";
-
-// const url_api3 = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyA8bWDWKfJ5UztmwkbAB4XiT8Md30xjV50&maxResults=40`;
-
-// // GET https://books.googleapis.com/books/v1/volumes?q=harry%20potter&key=[YOUR_API_KEY] HTTP/1.1
-
-const getData = async (url) => {
-    const response = await fetch(url);
+const getData = async (searchValue) => {
+    const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchValue}&maxResults=5`,
+    );
     const data = await response.json();
+    // console.log(data.items);
 
-    return data;
+    // const { volumeInfo } = data.items;
+    // console.log(volumeInfo.title);
+
+    // data.items.map((e) => {
+    //     return {
+    //         title: e.volumeInfo.title,
+    //     };
+    // });
+
+    return data.items.map((e) => {
+        return {
+            image: e.volumeInfo.imageLinks.thumbnail || "image unavaiable",
+            title: e.volumeInfo.title,
+            authors: e.volumeInfo.authors.join(",") || "authors unavaiable",
+            description: e.volumeInfo.description,
+        };
+    });
+    // return data.items.forEach((key) => {
+    //     key.volumeInfo.title;
+    // });
 };
 
-console.log(getData(url_api2));
+document.getElementById("btn").addEventListener("click", async () => {
+    const inf = await getData(search.value);
+    console.log(inf);
 
-// const change = () => {
-//     cns;
-// };
+    // const title = document.createElement("h2");
+    // title.classList.add("title");
+    // title.innerText = getData().title;
+
+    // document.getElementById("title").value = getData(search.value).title;
+});
+
+// const card = document.createElement('div').classList('card');
+// card.appendChild(document.createElement('h2').add('title'));
+
+const title = document.createElement("h3");
+title.classList.add("title");
+title.innerHTML = getData().title;
+
+const authors = document.createElement("p");
+authors.classList.add("authors");
+
+const description = document.createElement("p");
+description.classList.add("description");
